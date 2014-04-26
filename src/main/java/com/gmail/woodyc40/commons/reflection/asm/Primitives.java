c19b8d36e53965c8396137f73455f03da0192f15
AgentTroll <woodyc40@gmail.com>
2014-04-26 12:43:19 -0700

    Update Primitives.java

--- src/main/java/com/gmail/woodyc40/commons/reflection/asm/Primitives.java
+++ src/main/java/com/gmail/woodyc40/commons/reflection/asm/Primitives.java
@@ -3,7 +3,18 @@ package com.gmail.woodyc40.commons.reflection.asm;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Utility to get JVM letter representations for primitives
+ * 
+ * @author AgentTroll
+ * @version 1.0
+ */ 
 public class Primitives {
+    private Primitives() {}
+    
+    /**
+     * The primitive descriptors
+     */ 
     private static final Map<Class<?>, String> DESC_MAP = new HashMap<>();
 
     static {
@@ -19,6 +30,13 @@ public class Primitives {
         DESC_MAP.put(void.class, "V");
     }
 
+    /**
+     * Gets the descriptor associated with the primitive type
+     * 
+     * @param primitive the <code>class</code> representing a primitive type
+     * @return the JVM descriptor for the primitive, or <code>null</code> if 
+     *         not registered or not a primitive type
+     */ 
     public static String getDesc(Class<?> primitive) {
         return DESC_MAP.get(primitive);
     }
