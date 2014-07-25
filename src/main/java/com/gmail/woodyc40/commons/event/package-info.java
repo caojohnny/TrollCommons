eba6fe48077f700121a5ef63ca98e7c79b092097
AgentTroll <woodyc40@gmail.com>
2014-07-25 13:07:22 -0700

    Create package-info.java

---
+++ src/main/java/com/gmail/woodyc40/commons/event/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Event framework used to listen and call for events that are not suited for use by using the BukkitAPI
+ * {@link org.bukkit.plugin.PluginManager}.
+ *
+ * <p>
+ * This event framework is thread safe.
+ */
+
+package com.gmail.woodyc40.commons.event;
