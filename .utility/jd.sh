c129dc27c89716afbe9adbcc39d45c4b145a956c
AgentTroll <woodyc40@gmail.com>
2014-08-09 14:45:19 -0700

    Update jd.sh

--- .utility/jd.sh
+++ .utility/jd.sh
@@ -4,24 +4,24 @@
 
 # Makes sure it is not a PR
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-# Set it up
-cd $TRAVIS_BUILD_DIR
-git config --global user.email "woodyc40@gmail.com"
-git config --global user.name "AgentTroll"
-mkdir gh-pages
-
-# We're gonna make this a proper repo
-cd gh-pages
-git init
-
-# Lets commit some files
-cd $TRAVIS_BUILD_DIR
-mvn clean javadoc:javadoc
-
-# Push!
-cd gh-pages
-git add .
-git commit -m "Auto-publishing on successful travis build $TRAVIS_BUILD_NUMBER"
-git push -fq https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/AgentTroll/BukkitCommons.git HEAD:gh-pages
-echo -e "Published JavaDoc.\n" # Done!
+  # Set it up
+  cd $TRAVIS_BUILD_DIR
+  git config --global user.email "woodyc40@gmail.com"
+  git config --global user.name "AgentTroll"
+  mkdir gh-pages
+  
+  # We're gonna make this a proper repo
+  cd gh-pages
+  git init
+  
+  # Lets commit some files
+  cd $TRAVIS_BUILD_DIR
+  mvn clean javadoc:javadoc
+  
+  # Push!
+  cd gh-pages
+  git add .
+  git commit -m "Auto-publishing on successful travis build $TRAVIS_BUILD_NUMBER"
+  git push -fq https://AgentTroll:${PASS}@github.com/AgentTroll/BukkitCommons.git HEAD:gh-pages
+  echo -e "Published JavaDoc.\n" # Done!
 fi
